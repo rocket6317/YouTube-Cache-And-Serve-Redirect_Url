c43fb79a12daad5de6d648af0c2452b9f0383494
rocket6317 <108830817+rocket6317@users.noreply.github.com>
2025-11-15 22:08:50 +0000

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ def start_scheduler():
     # Refresh streams every UPDATE_INTERVAL_HOURS
     scheduler.add_job(process_channels, 'interval', hours=UPDATE_INTERVAL_HOURS)
 
-    # Prune logs once a day (midnight UTC)
+    # Prune logs once a day at midnight UTC
     scheduler.add_job(prune_old_logs, 'cron', hour=0, minute=0)
 
     scheduler.start()
