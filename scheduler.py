709dff23dc02f2d7a34357e817b62e4d4b2e122d
rocket6317 <108830817+rocket6317@users.noreply.github.com>
2025-11-15 22:59:25 +0000

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from fetcher import process_channels
-from db import set_last_updated
+from db import prune_old_logs
 from config import UPDATE_INTERVAL_HOURS
 
 scheduler = BackgroundScheduler()
 
 def start_scheduler():
     scheduler.add_job(refresh_streams, 'interval', hours=UPDATE_INTERVAL_HOURS)
+    scheduler.add_job(prune_logs, 'cron', hour=0, minute=0)  # Midnight UTC
     scheduler.start()
 
 def refresh_streams():
     process_channels()
-    set_last_updated()
+
+def prune_logs():
+    prune_old_logs(days=7)
